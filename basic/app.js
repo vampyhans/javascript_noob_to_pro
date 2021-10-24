53f8065c07953ce2659b8247a3a635551d7b5fb1
unknown <hansahimasarawijesinghe@gmail.com>
2021-10-24 14:39:37 +0530

    basic

---
+++ basic/app.js
@@ -0,0 +1,17 @@
+//we can change value of let
+
+let age = 24;
+let year = 2021;
+
+// we cant change value of const
+
+const yob = 1997;
+const firstName = 'Hansa';
+const lastName = 'Himasara';
+
+//outdated
+
+var date = 24
+
+console.log(age, year, yob, date);
+console.log(firstName + " " + lastName, firstName[0], firstName.length);
